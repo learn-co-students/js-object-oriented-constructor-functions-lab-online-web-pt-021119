a10f75d707c7eec095990873f3dd9531ce95f1cf
github username <github email address>
2019-08-27 12:40:44 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter (prop1, prop2, prop3){
+  this.year = prop1;
+  this.color = prop2;
+  this.model = prop3;
+}
+
+function Driver (name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation (address, city) {
+  this.address = address;
+  this.city = city;
+}
